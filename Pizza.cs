b9e2060354c145a5a06b5adcbab2c9ed0bfa6b92
Daniel Echeverri <echeverri121@gmail.com>
2024-01-22 10:05:42 -0500

    Create DB Context

--- Pizza.cs
+++ Pizza.cs
@@ -8,4 +8,10 @@ namespace PizzaStore.Models
           public string? Name { get; set; }
           public string? Description { get; set; }
     }
+    
+    class PizzaDb : DbContext
+    {
+        public PizzaDb(DbContextOptions options) : base(options) { }
+        public DbSet<Pizza> Pizzas { get; set; } = null!;
+    }
 }
